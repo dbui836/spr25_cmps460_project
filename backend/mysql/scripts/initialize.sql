e12b72a044fa467a7d86433bf2de00855131393f
dbui836 <dannybui2002@gmail.com>
2025-02-28 12:06:46 -0600

    Changed initialize.sql to account for on-delete operations

--- backend/mysql/scripts/initialize.sql
+++ backend/mysql/scripts/initialize.sql
@@ -4,12 +4,12 @@
 
 
 -- Initialize tables
-/*
+
 CREATE TABLE Pilot (
     pilotID INT PRIMARY KEY,
     pilot_fname VARCHAR(100) NOT NULL,
     pilot_lname VARCHAR(100) NOT NULL,
-    certs SET("Student", "Recreational", "Private", "Commercial", "Airline Transport") NOT NULL,
+    certs ENUM("Student", "Recreational", "Private", "Commercial", "Airline Transport") NOT NULL,
     pilot_location VARCHAR(100) NOT NULL,
     pilot_hrs_flown INT NOT NULL,
     CHECK (pilot_hrs_flown >= 0)
@@ -19,6 +19,8 @@ CREATE TABLE Pilot (
 CREATE TABLE Plane (
 
     model VARCHAR(100) NOT NULL,
+    reqCert ENUM("Student", "Recreational", "Private", "Commercial", "Airline Transport") NOT NULL,
+    pilot_location VARCHAR(100) NOT NULL,
     planeID INT PRIMARY KEY,
     plane_hrs_flown INT NOT NULL,
     max_capacity INT NOT NULL,
@@ -36,9 +38,15 @@ CREATE TABLE Flight (
     pilot1ID INT NOT NULL,
     pilot2ID INT,
 
-    FOREIGN KEY (planeID) REFERENCES Plane(planeID),
-    FOREIGN KEY (pilot1ID) REFERENCES Pilot(pilotID),
-    FOREIGN KEY (pilot2ID) REFERENCES Pilot(pilotID),
+    FOREIGN KEY (planeID) REFERENCES Plane(planeID)
+        ON DELETE RESTRICT
+    ,
+    FOREIGN KEY (pilot1ID) REFERENCES Pilot(pilotID)
+        ON DELETE RESTRICT
+    ,
+    FOREIGN KEY (pilot2ID) REFERENCES Pilot(pilotID)
+        ON DELETE SET NULL
+    ,
 
     seatAval INT NOT NULL,
     CHECK (seatAval >= 0)
@@ -57,7 +65,7 @@ CREATE TABLE Passenger (
     seat VARCHAR(5),
 
     FOREIGN KEY (flightID) REFERENCES Flight(flightID)
+        ON DELETE SET NULL
 
 );
 
-*/
